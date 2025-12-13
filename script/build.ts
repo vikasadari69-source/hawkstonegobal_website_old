d9540a4e43aa891b8aed51d1841381eaf1b9ed39
vikasadari69-source <vikasadari69@gmail.com>
2025-12-13 23:27:36 +0530

    update 1

--- script/build.ts
+++ script/build.ts
@@ -61,7 +61,11 @@ async function buildAll() {
     banner: {
       js: `
 import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 `,
     },
   });
